5959c38abd9c182d79a4f63d464a05fce9750de9
Rainer Gerhards <rgerhards@adiscon.com>
2018-01-27 18:23:34 +0100

    restructure repo to support stable and daily builds

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+echo =============== BUILDING PACKAGES FOR $1 ====================
+$SUDO rm -rf ~/.abuild/packages/*
+for dir in $( ls $1 ); do
+	if [ -e $1/$dir/APKBUILD ]; then
+		echo ==================== building $1/$dir
+		(cd $1/$dir; source ../../run)
+	fi
+done
+
+echo ==================== BUILD RESULT:
+find ~/.abuild/packages
